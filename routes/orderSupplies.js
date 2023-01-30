a657480553bb66cbb8cf63b3578db8c60ebda482
Scarrupatorio <57802344+Scarrupatorio@users.noreply.github.com>
2023-01-30 04:23:53 +0100

    create le fucking tabelle dinamiche

--- routes/orderSupplies.js
+++ routes/orderSupplies.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('../schemas/Item');
+var TitlesO = require('../schemas/TitoliTableOrder');
 
 /* GET pagina ordini. */
 router.get('/orders', function (req, res, next) {
-    res.render('ordersupplies');
+    TitlesO.find({}, {nome: 1}, function(err, title) {
+        if (err) throw err;
+        Item.find({},{nome: 1, prezzo: 1, quantità: 1}, function(err, item) {
+            if (err) throw err;
+            res.render('ordersupplies', {title: title, item: item});
+        });
+    });
 });
 
 module.exports = router;

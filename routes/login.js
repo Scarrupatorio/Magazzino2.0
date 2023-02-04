a5c0fe7c9f28755c22d241b7dc0497b5b9bcdfa0
Scarrupatorio <57802344+Scarrupatorio@users.noreply.github.com>
2023-02-04 03:03:46 +0100

    fixed login

--- routes/login.js
+++ routes/login.js
@@ -3,19 +3,19 @@ var router = express.Router();
 var Log = require('../schemas/Login');
 
 /* GET login. */
-router.get('/login', function (req, res, next) {
-    res.render('login');
+router.get('/', function (req, res, next) {
+    res.render('login',{text: ''});
 });
 
-router.post('/auth', function(req, res) {
+router.post('/index', function(req, res) {
     Log.findOne({},{username: 1, password: 1}, function(err,credential){
         console.log(credential.username + credential.password);
         if (credential.username == req.body.user){
             if(credential.password == req.body.pass){
                 console.log("logged");
-                res.render('');
-            }else {res.render('login');}
-        }else{res.render('login');}
+                res.render('index');
+            }else {res.render('login',{text: "credenziali errate"});}
+        }else{res.render('login',{text: "credenziali errate"});}
     })
 })
 
